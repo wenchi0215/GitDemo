1b8db40084dcdf1001f6635c61704c7ad6e54e23
Chi 張 <miqi2152969@gmail.com>
2018-03-08 15:29:50 +0800

    add Class1.cs and add cat

--- GitDemo/Class1.cs
+++ GitDemo/Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitDemo
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("cat");
-        }
     }
 }
